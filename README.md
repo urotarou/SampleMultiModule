f784f19f200920eacddcf6f52c3a0d0ed8ab8287
urotarou <devak9kflab@gmail.com>
2022-04-17 18:00:05 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Android Multi Module サンプル
---
+++ app/src/main/java/lab/uro/kitori/samplemultimodule/SampleMultiModuleApplication.kt
@@ -0,0 +1,7 @@
+package lab.uro.kitori.samplemultimodule
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SampleMultiModuleApplication : Application()
